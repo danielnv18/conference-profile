906f25ac843c957aa0a44b88089e56a116f12ae5
Daniel Noyola <danielnv18@gmail.com>
2018-08-28 14:21:09 -0600

    Added dependecies.

---
+++ modules/conference_schedule/assets/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  externals: {
+    jquery: 'jQuery',
+    drupal: 'Drupal',
+    drupalSettings: 'drupalSettings',
+  },
+};
