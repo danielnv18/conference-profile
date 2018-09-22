b5e30f9a2127c1284fa94bba9e2fc53e48c7cecb
Daniel Noyola <danielnv18@gmail.com>
2018-09-21 20:37:45 -0600

    Lint board component

--- modules/conference_schedule/assets/src/layout/Board.js
+++ modules/conference_schedule/assets/src/layout/Board.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import { DragDropContext } from "react-beautiful-dnd";
-import styled from "styled-components";
-import Column from "../componets/Column";
-import querySchedule from "./../hoc/SessionsQuery";
-import find from "lodash.find";
-import findindex from "lodash.findindex";
+import React from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+import find from 'lodash.find';
+import findindex from 'lodash.findindex';
+import Column from '../componets/Column';
+import querySchedule from '../hoc/SessionsQuery';
 
 const Grid = styled.section`
   display: grid;
@@ -12,14 +12,14 @@ const Grid = styled.section`
   grid-gap: 15px;
 `;
 
-class Board extends Component {
+class Board extends React.Component {
   constructor(props) {
     super(props);
 
-    let columns = [
+    const columns = [
       {
-        id: "un-schedule",
-        title: "Un-schedule",
+        id: 'un-schedule',
+        title: 'Un-schedule',
         date: null,
         sessionsIds: [],
         timeSlotIds: []
@@ -29,7 +29,7 @@ class Board extends Component {
       columns.push({
         id: date.toISOString(),
         title: date.toDateString(),
-        date: date,
+        date,
         sessionsIds: [],
         timeSlotIds: []
       });
@@ -38,36 +38,16 @@ class Board extends Component {
     this.state = { columns };
 
     this.assignSessions = this.assignSessions.bind(this);
-    this.assignTimeSlots = this.assignTimeSlots.bind(this);
     this.onDragEnd = this.onDragEnd.bind(this);
   }
 
   componentDidMount() {
-    let { columns } = this.state;
+    const { columns } = this.state;
 
     columns.forEach(this.assignSessions);
 
     this.setState({ columns });
   }
-
-  assignSessions(column, index, columns) {
-    const { sessions } = this.props;
-    let sessionsIds;
-    if (column.id === "un-schedule") {
-      sessionsIds = sessions.map(session => {
-        if (session.fieldTimeSlot === null) {
-          return session.uuid;
-        }
-      });
-    } else {
-    }
-
-    if (typeof sessionsIds !== "undefined") {
-      columns[index] = { ...column, sessionsIds };
-    }
-  }
-
-  assignTimeSlots() {}
 
   onDragEnd(result) {
     const { destination, source, draggableId } = result;
@@ -79,19 +59,16 @@ class Board extends Component {
     }
 
     // If the destination is the same as the source.
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) {
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
       return;
     }
 
-    const start = find(columns, ["id", source.droppableId]);
-    const finnish = find(columns, ["id", destination.droppableId]);
+    const start = find(columns, ['id', source.droppableId]);
+    const finnish = find(columns, ['id', destination.droppableId]);
 
     // Moving within the same column.
     if (start === finnish) {
-      const index = findindex(columns, ["id", source.droppableId]);
+      const index = findindex(columns, ['id', source.droppableId]);
       const newSessionIds = Array.from(start.sessionsIds);
       newSessionIds.splice(source.index, 1);
       newSessionIds.splice(destination.index, 0, draggableId);
@@ -121,8 +98,8 @@ class Board extends Component {
       sessionsIds: finishSessionIds
     };
 
-    const indexStart = findindex(columns, ["id", source.droppableId]);
-    const indexFinish = findindex(columns, ["id", destination.droppableId]);
+    const indexStart = findindex(columns, ['id', source.droppableId]);
+    const indexFinish = findindex(columns, ['id', destination.droppableId]);
 
     let newColumns = null;
     newColumns = columns.splice(indexStart, 1, newStart);
@@ -130,22 +107,31 @@ class Board extends Component {
     this.setState({ column: newColumns });
   }
 
+  assignSessions(column, index, columns) {
+    const { sessions, timeSlots } = this.props;
+    let sessionsIds;
+    if (column.id === 'un-schedule') {
+      sessionsIds = sessions.map(session => {
+        if (session.fieldTimeSlot === null) {
+          return session.uuid;
+        }
+      });
+    }
+
+    if (typeof sessionsIds !== 'undefined') {
+      columns[index] = { ...columns[index], sessionsIds };
+    }
+  }
+
   render() {
     const { sessions } = this.props;
+    const { columns } = this.state;
     return (
-      <Grid columns={this.state.columns.length}>
+      <Grid columns={columns.length}>
         <DragDropContext onDragEnd={this.onDragEnd}>
-          {this.state.columns.map(column => {
-            const columnSessions = column.sessionsIds.map(uuid =>
-              find(sessions, ["uuid", uuid])
-            );
-            return (
-              <Column
-                key={column.id}
-                column={column}
-                sessions={columnSessions}
-              />
-            );
+          {columns.map(column => {
+            const columnSessions = column.sessionsIds.map(uuid => find(sessions, ['uuid', uuid]));
+            return <Column key={column.id} column={column} sessions={columnSessions} />;
           })}
         </DragDropContext>
       </Grid>
